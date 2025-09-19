644ad479c6f2f555b889c17e9071b5e9e0733b32
Fatemeh-njf <152619102+fatemeh-nn@users.noreply.github.com>
2025-09-19 15:10:44 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css"
+import MyBanner from "./Components/MyBanner";
+
+
+
+
+function App(){
+return(
+  <div>
+  <MyBanner/>
+ 
+  </div>
+)
+}
+ export default App;
